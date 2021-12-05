6a25023f8ebf9874d65d574b3b2720b64dd546c9
Matthew Barrington <edeninflames@gmail.com>
2021-12-05 12:47:22 +0000

    day 3 part 1 redux

--- day3/main.go
+++ day3/main.go
@@ -3,168 +3,53 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func transpose(s [][]string) [][]string {
-	height := len(s)
-	width := len(s[0])
-	newSlice := make([][]string, width)
-
-	for i := 0; i < width; i++ {
-		newSlice[i] = make([]string, height)
-		for j := 0; j < height; j++ {
-			newSlice[i][j] = s[j][i]
-		}
+func makeDecimal(s []int64) float64 {
+	var end float64 = 0
+	for i := range s {
+		end += float64(s[i]) * math.Pow(2.0, float64(len(s)-i-1))
 	}
-	return newSlice
-}
-
-func tally(s []string) map[string]int {
-	count := make(map[string]int)
-
-	for _, x := range s {
-		count[x]++
-	}
-
-	return count
-}
-
-func mostCommon(s []string) (string, int) {
-	var mostCommonKey string
-
-	count := make(map[string]int)
-
-	for _, x := range s {
-		count[x]++
-	}
-
-	for k, v := range count {
-		if v > count[mostCommonKey] {
-			mostCommonKey = k
-		}
-	}
-
-	return mostCommonKey, count[mostCommonKey]
-}
-
-func leastCommon(s []string) (string, int) {
-	var leastCommonKey string
-
-	count := make(map[string]int)
-
-	for _, x := range s {
-		count[x]++
-	}
-
-	for k, v := range count {
-		if count[leastCommonKey] == 0 {
-			leastCommonKey = k
-		} else if v < count[leastCommonKey] {
-			leastCommonKey = k
-		}
-	}
-
-	return leastCommonKey, count[leastCommonKey]
-}
-
-func makeDecimal(s []string) int64 {
-	num, _ := strconv.ParseInt(strings.Join(s, ""), 2, 0)
-	return num
-}
-
-func removeLines(bits [][]string, index int, method string) [][]string {
-	if len(bits) == 1 {
-		return bits
-	}
-	eugh := transpose(bits)
-
-	var s string
-
-	mCommon, mCount := mostCommon(eugh[index])
-	lCommon, lCount := leastCommon(eugh[index])
-
-	if mCount > lCount {
-		s = mCommon
-	} else {
-		s = lCommon
-	}
-
-	if mCount == len(eugh[index])/2 && lCount == len(eugh[index])/2 {
-		if method == "most" {
-			s = "1"
-		} else {
-			s = "0"
-		}
-	}
-
-	if len(bits) == 20000 {
-		fmt.Printf("\nindex: %v\n", index)
-		fmt.Printf("s: %v\n", s)
-		fmt.Printf("bits: %v\n", bits)
-		fmt.Printf("eugh: %v\n", eugh)
-		fmt.Printf("method: %v\n", method)
-		fmt.Printf("mCount: %v\n", mCount)
-		fmt.Printf("lCount: %v\n", lCount)
-		fmt.Printf("mCommon: %v\n", mCommon)
-		fmt.Printf("lCommon: %v\n\n", lCommon)
-	}
-
-	stripped := [][]string{}
-
-	for _, number := range bits {
-		if number[index] == s {
-			stripped = append(stripped, number)
-		}
-	}
-
-	fmt.Printf("stripped: %v\n", stripped)
-
-	return stripped
+	return end
 }
 
 func main() {
-	file, _ := os.Open("test.txt")
+	file, _ := os.Open("input.txt")
 
 	scanner := bufio.NewScanner(file)
 
-	input := [][]string{}
+	input := [][]int64{}
 
-	gamma := []string{}
-	epsilon := []string{}
+	gamma := []int64{}
+	epsilon := []int64{}
 
 	for scanner.Scan() {
-		input = append(input, strings.Split(scanner.Text(), ""))
+		bits := []int64{}
+		line := strings.Split(scanner.Text(), "")
+		for _, x := range line {
+			bit, _ := strconv.ParseInt(x, 2, 0)
+			bits = append(bits, bit)
+		}
+		input = append(input, bits)
 	}
 
-	oxygen := input
-	co2 := input
-
-	transposed := transpose(input)
-
-	for i, x := range transposed {
-		count := tally(x)
-		fmt.Println(count)
-		most, _ := mostCommon(x)
-		least, _ := leastCommon(x)
-
-		//fmt.Printf("oxygen %v\n", oxygen)
-		//fmt.Printf("co2 %v\n", co2)
-		//fmt.Printf("i %v\n", i)
-		//fmt.Printf("most %v\n", most)
-
-		oxygen = removeLines(oxygen, i, "most")
-		co2 = removeLines(co2, i, "least")
-
-		gamma = append(gamma, most)
-		epsilon = append(epsilon, least)
+	for x := 0; x < len(input[0]); x++ {
+		count := make(map[int64]int)
+		for y := 0; y < len(input); y++ {
+			count[input[y][x]] += 1
+		}
+		if count[1] > count[0] {
+			gamma = append(gamma, 1)
+			epsilon = append(epsilon, 0)
+		} else {
+			gamma = append(gamma, 0)
+			epsilon = append(epsilon, 1)
+		}
 	}
 
-	fmt.Println(makeDecimal(oxygen[0]))
-	fmt.Println(makeDecimal(co2[0]))
-
-	fmt.Printf("part 1: %d\n", makeDecimal(gamma)*makeDecimal(epsilon))
-	fmt.Printf("part 2: %d\n", makeDecimal(oxygen[0])*makeDecimal(co2[0]))
+	fmt.Printf("part 1: %.0f\n", makeDecimal(gamma)*makeDecimal(epsilon))
 }
