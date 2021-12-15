14d2cb9b267e5721647e8cdbdc8a8309347d4158
Matthew Barrington <edeninflames@gmail.com>
2021-12-15 21:57:03 +0000

    Day 14 part 2.

--- day14/main.go
+++ day14/main.go
@@ -8,54 +8,68 @@ import (
 	"strings"
 )
 
-func tally(s []string) map[string]int {
-	output := map[string]int{}
+func getCount(lines []string, interations int) int {
+	transformMap := map[string][]string{}
 
-	for _, x := range s {
-		output[x]++
+	start := lines[0]
+	split := strings.Split(start, "")
+
+	for _, x := range lines[2:] {
+		chars := strings.Split(x, "")
+		transformMap[strings.Join(chars[0:2], "")] = []string{fmt.Sprintf("%s%s", chars[0], chars[6]), fmt.Sprintf("%s%s", chars[6], chars[1])}
 	}
 
-	return output
+	codes := map[string]int{}
+	first := split[0]
+	last := split[len(split)-1]
+
+	for i := 0; i < len(split)-1; i++ {
+		codes[strings.Join(split[i:i+2], "")]++
+	}
+
+	for i := 0; i < interations; i++ {
+		newCodes := map[string]int{}
+		for k, v := range codes {
+			for _, x := range transformMap[k] {
+				newCodes[x] += 1 * v
+			}
+		}
+		codes = newCodes
+	}
+
+	output := map[string]int{}
+
+	for k, v := range codes {
+		split := strings.Split(k, "")
+
+		for _, x := range split {
+			output[x] += v
+		}
+	}
+
+	output[first]++
+	output[last]++
+
+	counts := []int{}
+
+	for _, v := range output {
+		counts = append(counts, v/2)
+	}
+
+	sort.Ints(counts)
+
+	return counts[len(counts)-1] - counts[0]
 }
 
 func main() {
 	file, _ := os.Open("input.txt")
 	scanner := bufio.NewScanner(file)
 	lines := []string{}
-	transformMap := map[string]string{}
+
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
 
-	start := lines[0]
-
-	for _, x := range lines[2:] {
-		chars := strings.Split(x, "")
-		transformMap[strings.Join(chars[0:2], "")] = chars[6]
-	}
-
-	split := strings.Split(start, "")
-
-	for i := 0; i < 10; i++ {
-		newSplit := []string{split[0]}
-
-		for j := 0; j < len(split)-1; j++ {
-			key := strings.Join(split[j:j+2], "")
-			newSplit = append(newSplit, transformMap[key], split[j+1])
-		}
-
-		split = newSplit
-	}
-
-	vals := []int{}
-	info := tally(split)
-
-	for _, v := range info {
-		vals = append(vals, v)
-	}
-
-	sort.Ints(vals)
-
-	fmt.Println("part 1:", vals[len(vals)-1]-vals[0])
-
+	fmt.Println("part 1:", getCount(lines, 10))
+	fmt.Println("part 2:", getCount(lines, 40))
 }
